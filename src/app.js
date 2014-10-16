aa6ca8128ffe3f4726cb4b4fe78096c40d989b65
Neil Kim Gardose <neil.sprdout@gmail.com>
2014-10-16 14:31:47 +0800

    Add simple keyboard event

--- src/app.js
+++ src/app.js
@@ -9,12 +9,16 @@ var GameLayer = cc.Layer.extend({
 
     // Add your codes below...
     cc.log("Game initializing...");
+    // Add player with it's default properties set up.
     this.player = new Player(res.playerShip);
     this.player.attr({
       x: size.width * 0.5,
       y: size.height * 0.5
     });
     this.addChild(this.player, 0);
+
+    // Setting up keyboard and touch capability
+    this.gameControlSetup();
     // Add schedule on this layer, update method with delta time param.
     this.scheduleUpdate();
 
@@ -26,8 +30,22 @@ var GameLayer = cc.Layer.extend({
       deltaTime - time between the last time span.
   */
   update : function(dt) {
-    cc.log("Update has been called: " + dt);
+    // cc.log("Update has been called: " + dt);
   },
+  /*
+    Game control setup the keyboard and other inputs like touch
+    and mouse.
+  */
+  gameControlSetup: function() {
+    if(cc.sys.capabilities.hasOwnProperty('keyboard')) {
+      cc.eventManager.addListener({
+        event: cc.EventListener.KEYBOARD,
+        onKeyPressed: function(key, event) {
+          cc.log("Key pressed: " + key.toString());
+        }
+      }, this);
+    }
+  }
 });
 
 var GameScene = cc.Scene.extend({
